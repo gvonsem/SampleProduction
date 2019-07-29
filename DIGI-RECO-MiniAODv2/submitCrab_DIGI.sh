3c293672c790e0bfb9bfafff0046634d59fd6dc7
Gerrit Van Onsem <Gerrit.Patrick.Van.Onsem@cern.ch>
2019-07-29 13:24:33 +0200

    Fixing DIGI output dataset naming

--- DIGI-RECO-MiniAODv2/submitCrab_DIGI.sh
+++ DIGI-RECO-MiniAODv2/submitCrab_DIGI.sh
@@ -8,8 +8,19 @@ for DATASETIN in `cat datasets_GENSIM.txt`; do
   echo "DATASETIN: $DATASETIN"
   # a=("${(@s/-/)DATASETIN}")
   a=("${(@s|/|)DATASETIN}")
-  INSHORT=$a[2]-$a[3]
-  INSHORT=$a[2]
+  echo $a[2]
+  echo $a[3]
+#  INSHORT=$a[2]-$a[3]
+#  INSHORT=$a[2]
+    
+  INSHORT=("${(@s|_GEN-SIM_|)a[3]}")
+  INSHORT=("${(@s|gvonsem-|)INSHORT[1]}")
+#  echo "INSHORT = "$INSHORT
+  
+  ##remove leading whitespaces
+  INSHORT="${INSHORT##*( )}"
+#  echo "INSHORT = "$INSHORT  
+  
   CRABCONFIG=crabConfig_${INSHORT}.py
   echo "CRABCONFIG $CRABCONFIG"
   DATASETOUT=${INSHORT}_DIGI80X
