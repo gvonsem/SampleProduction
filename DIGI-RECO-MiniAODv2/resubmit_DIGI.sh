4d7626b94a2aa560d4b00f14a382c29b9861b506
Gerrit Van Onsem <Gerrit.Patrick.Van.Onsem@cern.ch>
2019-07-29 14:35:20 +0200

    Changing maximum memory for DIGI jobs

--- DIGI-RECO-MiniAODv2/resubmit_DIGI.sh
+++ DIGI-RECO-MiniAODv2/resubmit_DIGI.sh
@@ -4,5 +4,6 @@ for i in `ls crab_projects/|grep -v Mini|grep -v RECO`; do
   echo $i
   crab status -d crab_projects/$i
   #crab resubmit -d crab_projects/$i --maxjobruntime 1800 --maxmemory 2500
+  #crab resubmit -d crab_projects/$i --maxjobruntime 1800 --maxmemory 15200
   crab resubmit -d crab_projects/$i
 done
