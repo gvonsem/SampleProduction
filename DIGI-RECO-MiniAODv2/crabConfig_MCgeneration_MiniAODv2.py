ecd484c27610af7c920a8e96a0241b0a8692e478
Gerrit Van Onsem <Gerrit.Patrick.Van.Onsem@cern.ch>
2019-07-29 17:05:33 +0200

    MiniAODv2 crab scripts

--- DIGI-RECO-MiniAODv2/crabConfig_MCgeneration_MiniAODv2.py
+++ DIGI-RECO-MiniAODv2/crabConfig_MCgeneration_MiniAODv2.py
@@ -1,7 +1,7 @@
 from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 config = config()
 
-config.General.requestName = 'request_MiniAODv2_80X_INSHORT_DATE'
+config.General.requestName = 'request_MiniAODv2_94X_INSHORT_DATE'
 config.General.workArea = 'crab_projects'
 config.General.transferOutputs = True
 config.General.transferLogs = True
@@ -9,13 +9,16 @@ config.General.transferLogs = True
 # config.JobType.pluginName = 'PrivateMC'
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'config_MiniAODv2.py'
+config.JobType.numCores = 4
+config.JobType.maxMemoryMB = 5900
+config.JobType.allowUndistributedCMSSW = True
 
 config.Data.inputDataset = 'DATASETIN'
 config.Data.inputDBS = 'phys03'
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 1
 config.Data.totalUnits = config.Data.unitsPerJob * NJOBS
-config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
+config.Data.outLFNDirBase = '/store/group/cmst3/user/gvonsem/SampleProduction/'
 config.Data.publication = True
 config.Data.outputDatasetTag = 'DATASETOUT'
 config.Data.allowNonValidInputDataset = True
--- DIGI-RECO-MiniAODv2/resubmit_MiniAODv2.sh
+++ DIGI-RECO-MiniAODv2/resubmit_MiniAODv2.sh
@@ -3,5 +3,6 @@
 for i in `ls crab_projects/|grep Mini`; do
   echo $i
   crab status -d crab_projects/$i
-  crab resubmit -d crab_projects/$i  --maxjobruntime 1800 --maxmemory 2500
+  #crab resubmit -d crab_projects/$i  --maxjobruntime 1800 --maxmemory 2500
+  crab resubmit -d crab_projects/$i
 done
